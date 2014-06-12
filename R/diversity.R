23e45084b009585ef7d7cada1c3f3a7bff42806b
Kurt Hornik <Kurt.Hornik@R-project.org>
2014-06-12 00:00:00 +0000

    version 0.0-1

---
+++ R/diversity.R
@@ -0,0 +1,16 @@
+QDAP_Diversity_Annotator <-
+function()
+{
+    f <- function(s, a) {
+        s <- as.String(s)
+        diversity <- tail(unclass(diversity(s)), -2L)
+        Annotation(.document_id(a),
+                   "document",
+                   1L,
+                   nchar(s),
+                   list(list(diversity = diversity)))
+    }
+
+    Annotator(f,
+              "Document diversity annotator for English using qdap::diversity")
+}
---
+++ R/formality.R
@@ -0,0 +1,16 @@
+QDAP_Formality_Annotator <-
+function()
+{
+    f <- function(s, a) {
+        s <- as.String(s)
+        formality <- formality(s, progress.bar = FALSE)$formality$formality
+        Annotation(.document_id(a),
+                   "document",
+                   1L,
+                   nchar(s),
+                   list(list(formality = formality)))
+    }
+
+    Annotator(f,
+              "Document formality annotator for English using qdap::formality")
+}
